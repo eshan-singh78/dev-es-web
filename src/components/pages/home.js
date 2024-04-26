74c713b87d14e05994d7646f49a750dbd4399805
Eshan Singh <63192550+eshan-singh78@users.noreply.github.com>
2024-04-27 00:40:13 +0530

    social update

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaLaptop, FaDesktop, FaCode, FaGlobe, FaGithub, FaDiscord, FaYoutube, FaDocker } from 'react-icons/fa';
+import { FaLaptop, FaDesktop, FaCode, FaGlobe, FaGithub, FaDiscord, FaYoutube, FaDocker, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa';
 import './home.css';
 import placeholderImage from '../assets/homeimg.jpg';
 
@@ -44,7 +44,21 @@ function Home() {
         <img src={placeholderImage} alt="Placeholder" className="placeholder-image" />
         <div className="welcome-text">
           <h2>Hi there!</h2>
-          <p>Welcome to the official site of Dev Eshan. You all can leave your project ideas here, or can explore the site for a while!</p>
+          <p>Welcome to the official site of Dev Eshan. You all can leave your project ideas here, or can explore the site for a while! Btw below are the links to my social media which you can check via the icons!</p>
+          <div className="social-icons">
+        <a href="https://github.com/eshan-singh78" target="_blank" rel="noopener noreferrer" style={{ position: 'relative', right: '50px' }}>
+          <FaGithub className="icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/eshan-singh-3062501b1/" target="_blank" rel="noopener noreferrer" style={{ position: 'relative', left: '50px' }}>
+          <FaLinkedin className="icon" />
+        </a>
+        <a href="https://twitter.com/eshwnsingh" target="_blank" rel="noopener noreferrer" style={{ position: 'relative', right: '150px' }}>
+          <FaTwitter className="icon" />
+        </a>
+        <a href="https://instagram.com/e.shan_dev" target="_blank" rel="noopener noreferrer" style={{ position: 'relative', left: '150px' }}>
+          <FaInstagram className="icon" />
+        </a>
+      </div>
         </div>
       </div>
       {renderIcons()}
